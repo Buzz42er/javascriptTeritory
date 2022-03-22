14474e3333e6c4db7ee0c9dd0150a26b4ced1850
Buzz <jure.eres42@gmail.com>
2022-03-22 13:09:23 +0100

    spajanje file-ova

---
+++ learning.js
@@ -0,0 +1,20 @@
+const name = "Jure" ;
+var surname ;
+const years = 22 ;
+
+
+//this is a comment, it will be much more of those
+
+//data types
+Number = 3 ;
+String = "hello, this is a string"
+Array = '1', '2', '3' , '4' ;
+Boolean = true / false ;
+
+
+// math operations
+
+sum = years + 10;
+document.write(sum)
+//this is working now
+//I have conented html file with this one
